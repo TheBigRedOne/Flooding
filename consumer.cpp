3bc8467d4124e27534888e9a6be16ade697b4c1d
Yuting Wan <30644585+TheBigRedOne@users.noreply.github.com>
2024-06-26 14:43:35 +0100

    Update consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -1,53 +1,82 @@
 #include <ndn-cxx/face.hpp>
+#include <ndn-cxx/util/scheduler.hpp>
 #include <iostream>
+#include <unordered_set>
 #include <chrono>
 #include <thread>
+#include <boost/asio/io_context.hpp>
 
 class Consumer {
 public:
-    Consumer(const ndn::Name& interestName)
-    : m_interestName(interestName), m_face()
+    Consumer(const ndn::Name& interestName, int maxInflightInterests, int requestIntervalMs)
+    : m_interestName(interestName), m_face(m_ioContext), m_scheduler(m_ioContext), m_maxInflightInterests(maxInflightInterests), m_requestIntervalMs(requestIntervalMs)
     {
-        // 创建 Interest 对象
-        ndn::Interest interest(interestName);
-        interest.setInterestLifetime(ndn::time::milliseconds(1000));  // 设置兴趣包的生存时间为1000毫秒
-        interest.setMustBeFresh(true);  // 设置兴趣包要求返回最新的数据
-
-        m_face.expressInterest(interest,
-                               bind(&Consumer::onData, this, _1, _2),
-                               bind(&Consumer::onNack, this, _1, _2),
-                               bind(&Consumer::onTimeout, this, _1));
-        std::cout << "Interest sent for: " << interestName << std::endl;
+        sendInterests();
+        m_scheduler.schedule(ndn::time::milliseconds(m_requestIntervalMs), [this] { this->periodicRequest(); });
     }
 
     void run() {
         try {
-            m_face.processEvents();
+            m_ioContext.run();
         } catch (const std::exception& e) {
             std::cerr << "ERROR: " << e.what() << std::endl;
         }
     }
 
 private:
-    void onData(const ndn::Interest&, const ndn::Data& data) {
-        std::cout << "Received data: " << data << std::endl;
+    void periodicRequest() {
+        sendInterests();
+        m_scheduler.schedule(ndn::time::milliseconds(m_requestIntervalMs), [this] { this->periodicRequest(); });
     }
 
-    void onNack(const ndn::Interest&, const ndn::lp::Nack& nack) {
-        std::cout << "Received NACK: " << nack.getReason() << std::endl;
+    void sendInterests() {
+        while (m_inflightInterests.size() < m_maxInflightInterests) {
+            ndn::Name nextInterestName = m_interestName;
+            nextInterestName.appendSegment(m_nextSegment++);
+            ndn::Interest interest(nextInterestName);
+            interest.setInterestLifetime(ndn::time::milliseconds(1000));
+            interest.setMustBeFresh(true);
+
+            std::cout << "Sending Interest for: " << nextInterestName << std::endl;
+            m_inflightInterests.insert(nextInterestName.toUri());
+            m_face.expressInterest(interest,
+                                   bind(&Consumer::onData, this, _1, _2),
+                                   bind(&Consumer::onNack, this, _1, _2),
+                                   bind(&Consumer::onTimeout, this, _1));
+        }
+    }
+
+    void onData(const ndn::Interest& interest, const ndn::Data& data) {
+        std::cout << "Received data: " << data.getName() << std::endl;
+        m_inflightInterests.erase(interest.getName().toUri());
+    }
+
+    void onNack(const ndn::Interest& interest, const ndn::lp::Nack& nack) {
+        std::cout << "Received NACK for: " << interest.getName() << " reason: " << nack.getReason() << std::endl;
+        m_inflightInterests.erase(interest.getName().toUri());
     }
 
     void onTimeout(const ndn::Interest& interest) {
-        std::cout << "Timeout for interest: " << interest.getName() << std::endl;
+        std::cout << "Timeout for Interest: " << interest.getName() << std::endl;
+        m_inflightInterests.erase(interest.getName().toUri());
     }
 
+    boost::asio::io_context m_ioContext;
     ndn::Face m_face;
+    ndn::Scheduler m_scheduler{m_ioContext};
     ndn::Name m_interestName;
+    uint64_t m_nextSegment = 0;
+    int m_maxInflightInterests;
+    int m_requestIntervalMs;
+    std::unordered_set<std::string> m_inflightInterests;
 };
 
 int main(int argc, char* argv[]) {
-    ndn::Name interestName("/example/producer");  // 与生产者程序的前缀相对应
-    Consumer consumer(interestName);
+    ndn::Name interestName("/example/producer");
+    int maxInflightInterests = 5;
+    int requestIntervalMs = 5;
+
+    Consumer consumer(interestName, maxInflightInterests, requestIntervalMs);
     std::cout << "Consumer running, requesting data from " << interestName << std::endl;
     consumer.run();
 
